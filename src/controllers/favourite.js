f7eaa97e8f2e7900d35fd6fdfeecc8c120f4e2e2
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-05-06 14:46:16 +0200

    Added possibility to save favourite attractions

---
+++ src/controllers/favourite.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+const Attraction = require('../models/attraction');
+const attractionReducer = require('../helpers/attractionReducer');
+
+class FavouriteAttractionsController {
+  /**
+   * Returns list of all attractions marked as favourite
+   * @param {express.Response} res
+   * @param {express.NextFunction} next
+   */
+  static async all(req, res, next) {
+    // Check if the user is signed in
+    if (!req.isAuthenticated()) return res.status(401).json({ message: 'Not signed in' });
+
+    const favourites = req.user.favourites;
+
+    try {
+      // Find all attractions
+      const attractions = await Attraction.find({ _id: { $in: favourites } });
+      const reduced = Object.values(attractions).map((attraction) =>
+        attractionReducer(attraction._doc)
+      );
+
+      res.send(reduced);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * Adds an attraction to favourites listg
+   * @param {express.Response} res
+   * @param {express.NextFunction} next
+   */
+  static async add(req, res, next) {
+    // Check if the user is signed in
+    if (!req.isAuthenticated()) return res.status(401).json({ message: 'Not signed in' });
+
+    const id = req.body.id;
+
+    // Validate given id
+    if (!id || !mongoose.isValidObjectId(id))
+      return res.status(400).json({ message: 'Invalid ObjectId' });
+
+    try {
+      // Check if attraction exists
+      const attraction = await Attraction.findById(id);
+      if (!attraction) return res.status(404).json({ message: 'Attraction not found' });
+
+      const user = req.user;
+
+      // prettier-ignore
+      const alreadyExists = user.favourites.some((attraction) => attraction == id);
+
+      if (alreadyExists)
+        return res.status(200).json({ message: 'Attraction already in favourites' });
+
+      // Add attraction to favourites if it has not been already favourited
+      user.favourites.push(id);
+      await user.updateOne({ favourites: user.favourites }, { runValidators: true });
+
+      res.status(200).json({ message: 'Attraction added to favourites' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * Removes an attraction from favourites list
+   * @param {express.Response} res
+   * @param {express.NextFunction} next
+   */
+  static async remove(req, res, next) {
+    // Check if the user is signed in
+    if (!req.isAuthenticated()) return res.status(401).json({ message: 'Not signed in' });
+
+    const id = req.body.id;
+
+    // Validate given id
+    if (!id || !mongoose.isValidObjectId(id))
+      return res.status(400).json({ message: 'Invalid ObjectId' });
+
+    try {
+      const filtered = req.user.favourites.filter((attraction) => attraction != id);
+
+      if (filtered.length !== req.user.favourites.length) {
+        // Remove attraction from favourites
+        await req.user.updateOne({ favourites: filtered }, { runValidators: true });
+        return res.status(200).json({ message: 'Attraction removed from favourites' });
+      } else {
+        return res.status(404).json({ message: 'Attraction not found' });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = FavouriteAttractionsController;
--- src/routes/api/map.js
+++ src/routes/api/map.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AttractionsController = require('../../controllers/attractions');
+const FavouriteAttractionsController = require('../../controllers/favourite');
 
 const router = express.Router();
 
@@ -9,5 +10,9 @@ router.get('/attractions/:id', AttractionsController.getOne);
 router.post('/attractions', AttractionsController.create);
 router.put('/attractions/:id', AttractionsController.update);
 router.delete('/attractions/:id', AttractionsController.delete);
+
+router.get('/favourites', FavouriteAttractionsController.all);
+router.post('/favourites', FavouriteAttractionsController.add);
+router.delete('/favourites', FavouriteAttractionsController.remove);
 
 module.exports = router;
