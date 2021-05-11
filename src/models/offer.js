510019f4762887ab2b2b1ed51b42dfe5b351acbe
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-05-11 15:57:55 +0200

    Added update controller Added option to pass a custom reducer to the common controller

--- src/models/offer.js
+++ src/models/offer.js
@@ -2,10 +2,27 @@ const mongoose = require('mongoose');
 
 const offerSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  description: { type: String, required: true },
+  description: { type: String, required: false, default: null },
   price: { type: Number, required: true },
-  discountedPrice: { type: Number, required: true },
+  discountedPrice: {
+    type: Number,
+    required: true,
+    default: function () {
+      return this.price;
+    },
+  },
   image: { type: String, required: true },
+});
+
+offerSchema.pre('updateOne', function (next) {
+  const update = this.getUpdate();
+
+  // Set null if description is empty
+  if ('description' in update && !update.description?.trim()) {
+    update.description = null;
+  }
+
+  next();
 });
 
 const Offer = mongoose.model('offer', offerSchema);
